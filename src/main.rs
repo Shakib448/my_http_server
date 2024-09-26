44a7b91025c610c0ba0357308db7a37469678139
Shakib448 <shakiba448@gmail.com>
2024-09-26 21:52:51 +0600

    WIP on http request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    io::{BufRead, BufReader, Result},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_client(stream);
+    }
+
+    Ok(())
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buf_reader = BufReader::new(&mut stream);
+
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("The http request looks like : {http_request:#?}")
 }
